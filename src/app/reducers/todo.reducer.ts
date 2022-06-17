9e6a9cb785c6be385d7ffbdfd6c33006c6875016
ngactive99 <ngactive99@gmail.com>
2022-06-17 11:36:32 +0530

    Add todo functionality completed

--- src/app/reducers/todo.reducer.ts
+++ src/app/reducers/todo.reducer.ts
@@ -3,13 +3,14 @@ import { ActionParent } from '../actions/todo.actions';
 import { Todo } from '../models/Todo';
 
 const initialState: Todo[] = [
-  { title: 'Todo 1' },
-  { title: 'Todo 2' },
-  { title: 'Todo 3' },
+  { title: '' }
+  
 ];
 
 export function TodoReducer(state = initialState, action: ActionParent) {
   switch (action.type) {
+    case TodoActionType.Add: 
+    return [...state, action.payload]
     default:
       return state;
   }
